16876ccb8e95e8fc2b49cde523d80b11156230e1
Mark Russinovich <mark@Marks-MacBook-Pro.local>
2017-04-10 15:16:25 -0700

    Recording activity to Postgres DB

--- shopfinder/models.py
+++ shopfinder/models.py
@@ -32,3 +32,39 @@ class Shop(models.Model):
                 point = "POINT(%s %s)" % (latlon[1], latlon[0])
                 self.location = geos.fromstr(point)
         super(Shop, self).save()
+
+class SearchData(models.Model):
+    typedData = models.CharField(max_length=500)
+   # city = models.CharField(max_length=50) for later
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=5)
+    location = gis_models.PointField(u"longitude/latitude",
+                                     geography=True, blank=True, null=True)
+
+ #   def save(self, **kwargs):
+ #       if not self.location:
+ #           address = self.typedData
+ #           address = address.encode('utf-8')
+ #           geocoder = Bing(api_key='AmTOjxQXZeKHxacoFI3IoPyR6PHW0U-r0NgDSQ8F-x0dt97mnLMFKREiS96mkOkX')
+ #           try:
+ #               location = geocoder.geocode(address)
+ #           except (URLError, GeocoderServiceError, ValueError):
+ #               pass
+ #           else:
+ #               addrArr = location.raw['displayName'].split(',')
+ #               self.state = addrArr[len(addrArr)-3]
+ #               self.zipcode = addrArr[len(addrArr)-2]
+ #               point = "POINT(%s %s)" % (location.latitude, location.longitude)
+ #               self.location = geos.fromstr(point)
+ #       super(SearchData, self).save()
+    
+    def __unicode__(self):
+        return self.typedData
+
+class ShopSearch(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    hit = models.ForeignKey(SearchData, on_delete=models.CASCADE)
+    dt = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.dt
--- shopfinder/views.py
+++ shopfinder/views.py
@@ -6,41 +6,58 @@ from django.shortcuts import render
 from django.template import RequestContext
 from geopy.geocoders import Bing
 from geopy.geocoders.bing import GeocoderServiceError
+from datetime import datetime
 
 from shopfinder import forms
-from shopfinder import models
+from shopfinder.models import Shop
+from shopfinder.models import SearchData
+from shopfinder.models import ShopSearch
 
 
 def geocode_address(address):
     address = address.encode('utf-8')
     geocoder = Bing(api_key='AmTOjxQXZeKHxacoFI3IoPyR6PHW0U-r0NgDSQ8F-x0dt97mnLMFKREiS96mkOkX')
     try:
-        _, latlon = geocoder.geocode(address)
+        location = geocoder.geocode(address)
     except (URLError, GeocoderServiceError, ValueError):
-        return None
+        pass
     else:
-        return latlon
+        return location
 
 def get_shops(longitude, latitude):
     current_point = geos.fromstr("POINT(%s %s)" % (longitude, latitude))
     distance_from_point = {'km': 10}
-    shops = models.Shop.gis.filter(location__distance_lte=(current_point, measure.D(**distance_from_point)))
+    shops = Shop.gis.filter(location__distance_lte=(current_point, measure.D(**distance_from_point)))
     shops = shops.distance(current_point).order_by('distance')
     return shops.distance(current_point)
+
+def save_search(rawAddr, locationDetails, shops):
+    point = "POINT(%s %s)" % (locationDetails.longitude, locationDetails.latitude)
+    loc = geos.fromstr(point)
+    state = locationDetails.raw['address']['adminDistrict'] #Error Handling
+    zipcode = locationDetails.raw['address']['postalCode']
+    typedData = SearchData(typedData=rawAddr, state=state, zipcode=zipcode, location=loc)
+    typedData.save()
+    dt = str(datetime.utcnow())
+    for shop in shops:
+        shopData = ShopSearch(shop=shop, hit=typedData, dt=dt)
+        shopData.save()
 
 def home(request):
     form = forms.AddressForm()
     shops = []
-    latitude = 0
-    longitude = 0
+    latitude = "0"
+    longitude = "0"
     if request.POST:
         form = forms.AddressForm(request.POST)
         if form.is_valid():
             address = form.cleaned_data['address']
             location = geocode_address(address)
             if location:
-                latitude, longitude = location
-                shops = get_shops(longitude, latitude)
+                shops = get_shops(location.longitude, location.latitude)
+                saveData = save_search(address, location, shops)
+                latitude = location.latitude
+                longitude = location.longitude
 
     return render(request,
         'home.html',
